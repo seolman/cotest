004bf1dc5cd4cac4af61503e179eafaf5640fa49
seolman <tjfehdgns@gmail.com>
2025-11-13 02:59:04 +0000

    init

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          gnumake
+          nodejs_latest
+          go
+          lua
+        ];
+      };
+    };
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/seolman/cotest
+
+go 1.25.2
