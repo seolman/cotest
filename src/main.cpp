327d11baef278577a0bef8b290f3f84fa99c039c
seolman <tjfehdgns@gmail.com>
2025-11-14 01:09:18 +0000

    next_permutation

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,8 @@
+#include <algorithm>
 #include <iostream>
-#include <string>
-#include <unordered_map>
 #include <vector>
 
 using namespace std;
-
-int solution(vector<vector<string>> clothes) {
-  unordered_map<string, int> clothes_map;
-  for (const auto &cloth_pair : clothes) {
-    clothes_map[cloth_pair[1]]++;
-  }
-  int answer = 1;
-  for (const auto &pair : clothes_map) {
-    // 안 입은 경우를 포함
-    answer *= (pair.second + 1);
-  }
-  // 아무 것도 안입은 경우 -1
-  return answer - 1;
-}
 
 template <typename T> ostream &operator<<(ostream &os, const vector<T> &vec) {
   os << "{ ";
@@ -31,24 +16,36 @@ template <typename T> ostream &operator<<(ostream &os, const vector<T> &vec) {
   return os;
 }
 
+int solution(int k, vector<vector<int>> dungeons) {
+  int answer = -1;
+  // next_permutation()은 오름차순 정렬이 되야함?
+  sort(dungeons.begin(), dungeons.end());
+  do {
+    int current_k = k;
+    int count = 0;
+    cout << dungeons << endl;
+    for (const auto &dungeon : dungeons) {
+      int required = dungeon[0];
+      int use = dungeon[1];
+      cout << "current k: " << current_k << ", " << "required: " << required << endl;
+      if (current_k >= required) {
+        current_k -= use;
+        count++;
+      } else {
+        break;
+      }
+    }
+
+    answer = max(count, answer);
+  } while (next_permutation(dungeons.begin(), dungeons.end()));
+  return answer;
+}
+
 int main() {
-  vector<vector<string>> clothes = {{"yellow_hat", "headgear"},
-                                    {"blue_sunglasses", "eyewear"},
-                                    {"green_turban", "headgear"}};
-  int expected = 5;
-  int result = solution(clothes);
-
-  if (result == expected) {
-    cout << "result: " << result << endl;
-  } else {
-    cout << "expected: " << expected << ", " << "result: " << result << endl;
-  }
-
-  clothes = {{"crow_mask", "face"},
-             {"blue_sunglasses", "face"},
-             {"smoky_makeup", "face"}};
-  expected = 3;
-  result = solution(clothes);
+  int k = 80;
+  vector<vector<int>> dungeons = {{80, 20}, {50, 40}, {30, 10}};
+  int expected = 3;
+  int result = solution(k, dungeons);
 
   if (result == expected) {
     cout << "result: " << result << endl;
