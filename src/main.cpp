4ffd3a8c144330d4e618b674d5c4251df8767b49
seolman <tjfehdgns@gmail.com>
2025-11-18 01:28:50 +0000

    2 queue

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
-#include <algorithm>
+#include <deque>
 #include <iostream>
-#include <unordered_map>
-#include <utility>
 #include <vector>
 
 using namespace std;
@@ -18,59 +16,58 @@ template <typename T> ostream &operator<<(ostream &os, const vector<T> &vec) {
   return os;
 }
 
-vector<int> solution(vector<string> genres, vector<int> plays) {
-  vector<int> answer;
-  unordered_map<string, int> genre_plays;
-  unordered_map<string, vector<pair<int, int>>> songs_by_genre;
-  for (int i = 0; i < genres.size(); i++) {
-    genre_plays[genres[i]] += plays[i];
-    songs_by_genre[genres[i]].push_back({plays[i], i});
-  }
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+  int answer = 0;
+  int total_weight = 0;
+  deque<int> trucks(truck_weights.begin(), truck_weights.end());
+  deque<int> bridge(bridge_length, 0);
 
-  vector<pair<int, string>> sorted_genre;
-  for (const auto &[genre, plays] : genre_plays) {
-    sorted_genre.push_back({plays, genre});
-  }
-  sort(sorted_genre.begin(), sorted_genre.end(),
-       [](pair<int, string> &a, pair<int, string> &b) {
-         return a.first > b.first;
-       });
+  while (!trucks.empty()) {
+    answer++;
 
-  for (const auto &[plays, genre] : sorted_genre) {
-    // cout << "genre: " << genre << endl;
-    auto &songs = songs_by_genre[genre];
-    sort(songs.begin(), songs.end(), [](pair<int, int> &a, pair<int, int> &b) {
-      if (a.first != b.first) {
-        return a.first > b.first;
-      }
-      return a.second < b.second;
-    });
+    total_weight -= bridge.front();
+    bridge.pop_front();
 
-    // for (const auto &s : songs) {
-    //   cout << "plays: " << s.first << ", " << "idx: " << s.second << endl;
-    // }
-    for (int i = 0; i < 2 && i < songs.size(); i++) {
-      answer.push_back(songs[i].second);
+    if (trucks.front() + total_weight <= weight) {
+      int new_truck = trucks.front();
+      trucks.pop_front();
+
+      bridge.push_back(new_truck);
+      total_weight += new_truck;
+    } else {
+      bridge.push_back(0);
     }
   }
-  return answer;
+
+  return answer + bridge_length;
 }
 
 int main() {
-  vector<string> genres = {"classic", "pop", "classic", "classic", "pop"};
-  vector<int> plays = {500, 600, 150, 800, 2500};
-  vector<int> expected = {4, 1, 3, 0};
-  vector<int> result = solution(genres, plays);
+  int bridge_length = 2;
+  int weight = 10;
+  vector<int> truck_weights = {7, 4, 5, 6};
+  int expected = 8;
+  int result = solution(bridge_length, weight, truck_weights);
   if (result == expected) {
     cout << "result: " << result << endl;
   } else {
     cout << "expected: " << expected << ", " << "result: " << result << endl;
   }
-
-  genres = {"classic", "pop", "classic", "classic", "pop", "pop"};
-  plays = {500, 600, 150, 800, 2500, 2500};
-  expected = {4, 5, 3, 0};
-  result = solution(genres, plays);
+  bridge_length = 100;
+  weight = 100;
+  truck_weights = {10};
+  expected = 101;
+  result = solution(bridge_length, weight, truck_weights);
+  if (result == expected) {
+    cout << "result: " << result << endl;
+  } else {
+    cout << "expected: " << expected << ", " << "result: " << result << endl;
+  }
+  bridge_length = 100;
+  weight = 100;
+  truck_weights = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+  expected = 110;
+  result = solution(bridge_length, weight, truck_weights);
   if (result == expected) {
     cout << "result: " << result << endl;
   } else {
