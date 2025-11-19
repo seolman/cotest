0ad5601db47c89a171d1d481804459a97b45a6fb
seolman <tjfehdgns@gmail.com>
2025-11-19 03:05:32 +0000

    double priority queue, split, multiset

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
-#include <algorithm>
-#include <functional>
 #include <iostream>
-#include <queue>
-#include <utility>
+#include <iterator>
+#include <set>
+#include <sstream>
+#include <string>
 #include <vector>
 
 using namespace std;
@@ -19,42 +19,85 @@ template <typename T> ostream &operator<<(ostream &os, const vector<T> &vec) {
   return os;
 }
 
-struct cmp {
-  bool operator()(pair<int, int> &a, pair<int, int> &b) {
-    return a.second > b.second;
+class DoubleQueue {
+public:
+  void push(int value) { ms.insert(value); }
+
+  void pop_min() {
+    if (!ms.empty()) {
+      ms.erase(ms.begin());
+    }
   }
+
+  void pop_max() {
+    if (!ms.empty()) {
+      ms.erase(prev(ms.end()));
+    }
+  }
+
+  int min() const {
+    if (!ms.empty()) {
+      return *ms.begin();
+    }
+    return 0;
+  }
+
+  int max() const {
+    if (!ms.empty()) {
+      return *ms.rbegin();
+    }
+    return 0;
+  }
+
+  bool empty() { return ms.empty(); }
+
+private:
+  multiset<int> ms;
 };
 
-int solution(vector<vector<int>> jobs) {
-  int answer = 0;
-  sort(jobs.begin(), jobs.end());
+vector<string> split(const string &input, const char &delimiter) {
+  vector<string> result;
+  stringstream ss(input);
+  string segment;
 
-  priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq;
-  int idx = 0;
-  int time = 0;
-  while (idx < jobs.size() || !pq.empty()) {
-    if (idx < jobs.size() && jobs[idx][0] <= time) {
-      pq.push({jobs[idx][0], jobs[idx][1]});
-      idx++;
-      continue;
-    }
-
-    if (!pq.empty()) {
-      time += pq.top().second;
-      answer += time - pq.top().first;
-      pq.pop();
-    } else {
-      time = jobs[idx][0];
-    }
+  while (getline(ss, segment, delimiter)) {
+    result.push_back(segment);
   }
 
-  return answer / jobs.size();
+  return result;
+}
+
+vector<int> solution(vector<string> operations) {
+  DoubleQueue dpq;
+  for (const auto &o : operations) {
+    auto a = split(o, ' ');
+    string code = a[0];
+    int number = stoi(a[1]);
+    if (code == "I") {
+      dpq.push(number);
+    } else if (code == "D") {
+      if (dpq.empty()) {
+        continue;
+      }
+
+      if (number == 1) {
+        dpq.pop_max();
+      } else {
+        dpq.pop_min();
+      }
+    }
+  }
+  if (dpq.empty()) {
+    return {0, 0};
+  } else {
+    return {dpq.max(), dpq.min()};
+  }
 }
 
 int main() {
-  vector<vector<int>> jobs = {{0, 3}, {1, 9}, {3, 5}};
-  int expected = 8;
-  int result = solution(jobs);
+  vector<string> operations = {"I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"};
+  vector<int> expected = {0, 0};
+  vector<int> result = solution(operations);
   if (result == expected) {
     cout << "result: " << result << endl;
   } else {
