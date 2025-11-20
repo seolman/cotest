e501728d80fa103899ea133b60ff56f94cbc887f
seolman <tjfehdgns@gmail.com>
2025-11-20 02:03:31 +0000

    dfs, brute-force

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,5 @@
-#include <algorithm>
-#include <climits>
-#include <cmath>
-#include <cstdlib>
 #include <iostream>
-#include <queue>
+#include <string>
 #include <vector>
 
 using namespace std;
@@ -20,60 +16,37 @@ template <typename T> ostream &operator<<(ostream &os, const vector<T> &vec) {
   return os;
 }
 
-int bfs(const vector<vector<int>> &graph, int start, int ignore1, int ignore2,
-        int n) {
-  int count = 1;
-  queue<int> q;
-  vector<int> visited(n + 1, false);
-
-  q.push(start);
-  visited[start] = true;
-
-  while (!q.empty()) {
-    int current_node = q.front();
-    q.pop();
-
-    for (int n : graph[current_node]) {
-      if ((current_node == ignore1 && n == ignore2) ||
-          (current_node == ignore2 && n == ignore1)) {
-        continue;
-      }
-
-      if (!visited[n]) {
-        count++;
-        visited[n] = true;
-        q.push(n);
-      }
-    }
+void dfs(vector<string> &dictionary, char vowels[], string word) {
+  if (word.length() > 5) {
+    return;
   }
-  return count;
+  if (!word.empty()) {
+    dictionary.push_back(word);
+  }
+
+  for (int i = 0; i < 5; i++) {
+    dfs(dictionary, vowels, word + vowels[i]);
+  }
 }
 
-int solution(int n, vector<vector<int>> wires) {
-  int answer = INT_MAX;
-  vector<vector<int>> graph(n + 1);
-  for (const auto &w : wires) {
-    graph[w[0]].push_back(w[1]);
-    graph[w[1]].push_back(w[0]);
-  }
+int solution(string word) {
+  char vowels[] = {'A', 'E', 'I', 'O', 'U'};
+  vector<string> dictionary;
 
-  for (const auto &w : wires) {
-    int v1 = w[0];
-    int v2 = w[1];
+  dfs(dictionary, vowels, "");
 
-    int count1 = bfs(graph, v1, v1, v2, n);
-    int count2 = n - count1;
-    answer = min(answer, abs(count1 - count2));
+  for (int i = 0; i < dictionary.size(); i++) {
+    if (word == dictionary[i]) {
+      return i + 1;
+    }
   }
-  return answer;
+  return -1;
 }
 
 int main() {
-  int n = 9;
-  vector<vector<int>> wires = {{1, 3}, {2, 3}, {3, 4}, {4, 5},
-                               {4, 6}, {4, 7}, {7, 8}, {7, 9}};
-  int expected = 3;
-  int result = solution(n, wires);
+  string word = "AAAAE";
+  int expected = 6;
+  int result = solution(word);
   if (result == expected) {
     cout << "result: " << result << endl;
   } else {
