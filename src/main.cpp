8ec89dab8a96c27f573f8b34175b9fa410786200
seolman <tjfehdgns@gmail.com>
2025-11-25 05:51:49 +0000

    bfs, adj list

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,9 @@
 #include <algorithm>
 #include <iostream>
+#include <queue>
 #include <vector>
 
 using namespace std;
-
-int solution(int distance, vector<int> rocks, int n) {
-  sort(rocks.begin(), rocks.end());
-  int low = 1;
-  int high = distance;
-  int answer = 0;
-
-  while (low <= high) {
-    int mid = low + (high - low) / 2;
-    int removed = 0;
-    int pos = 0;
-
-    for (const auto &rock : rocks) {
-      if (rock - pos < mid) {
-        removed++;
-      } else {
-        pos = rock;
-      }
-    }
-
-    if (distance - pos < mid) {
-      removed++;
-    }
-
-    if (removed <= n) {
-      answer = mid;
-      low = mid + 1;
-    } else {
-      high = mid - 1;
-    }
-  }
-  return answer;
-}
 
 template <typename T> ostream &operator<<(ostream &os, vector<T> &vec) {
   os << "{";
@@ -49,14 +17,49 @@ template <typename T> ostream &operator<<(ostream &os, vector<T> &vec) {
   return os;
 }
 
+int solution(int n, vector<vector<int>> edge) {
+  int answer = 0;
+  vector<vector<int>> adj(n + 1);
+  vector<int> distance(n + 1, -1);
+
+  for (const auto &e : edge) {
+    adj[e[0]].push_back(e[1]);
+    adj[e[1]].push_back(e[0]);
+  }
+
+  queue<int> q;
+  distance[1] = 0;
+  q.push(1);
+
+  while (!q.empty()) {
+    int current = q.front();
+    q.pop();
+
+    for (const int &next : adj[current]) {
+      if (distance[next] == -1) {
+        q.push(next);
+        distance[next] = distance[current] + 1;
+      }
+    }
+  }
+
+  int max = *max_element(distance.begin() + 1, distance.end());
+  for (const auto &d : distance) {
+    if (max == d) {
+      answer++;
+    }
+  }
+  return answer;
+}
+
 int main() {
   cout << "TESTING" << endl;
 
-  int distance = 25;
-  vector<int> rocks = {2, 14, 11, 21, 17};
-  int n = 2;
-  int expected = 4;
-  int result = solution(distance, rocks, n);
+  int n = 6;
+  vector<vector<int>> edge = {{3, 6}, {4, 3}, {3, 2}, {1, 3},
+                              {1, 2}, {2, 4}, {5, 2}};
+  int expected = 3;
+  int result = solution(n, edge);
 
   if (result == expected) {
     cout << "result: " << result << endl;
