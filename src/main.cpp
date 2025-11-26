803ace32ca8bb58fd430020533362941bae20bb8
seolman <tjfehdgns@gmail.com>
2025-11-26 02:18:39 +0000

    dp, optimal structure, bottom-up
    triangle 배열 자체를 dp 테이블로 활용.
    전체 문제의 최적해는 그 부분 문제의 최적해로 이루어진다.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
+#include <algorithm>
 #include <iostream>
-#include <map>
-#include <unordered_set>
 #include <vector>
 
 using namespace std;
@@ -17,50 +16,29 @@ template <typename T> ostream &operator<<(ostream &os, vector<T> &vec) {
   return os;
 }
 
-int solution(int N, int number) {
-  if (N == number) {
-    return 1;
-  }
-
-  vector<unordered_set<int>> dp(9);
-
-  int concat_n = 0;
-  for (int i = 1; i < 9; i++) {
-    concat_n = concat_n * 10 + N;
-    dp[i].insert(concat_n);
-  }
-
-  for (int k = 2; k < 9; k++) {
-    for (int i = 1; i < k; i++) {
-      int j = k - i;
-
-      for (const int num1 : dp[i]) {
-        for (const int num2 : dp[j]) {
-          dp[k].insert(num1 + num2);
-          dp[k].insert(num1 - num2);
-          dp[k].insert(num1 * num2);
-          if (num2 != 0) {
-            dp[k].insert(num1 / num2);
-          }
-        }
+int solution(vector<vector<int>> triangle) {
+  for (int i = 1; i < triangle.size(); i++) {
+    for (int j = 0; j < triangle[i].size(); j++) {
+      if (j == 0) {
+        triangle[i][j] += triangle[i - 1][j];
+      } else if (j == triangle[i].size() - 1) {
+        triangle[i][j] += triangle[i - 1][j - 1];
+      } else {
+        triangle[i][j] += max(triangle[i - 1][j], triangle[i - 1][j - 1]);
       }
     }
-
-    if (dp[k].count(number)) {
-      return k;
-    }
   }
-
-  return -1;
+  int answer = *max_element(triangle.back().begin(), triangle.back().end());
+  return answer;
 }
 
 int main() {
   cout << "TESTING" << endl;
 
-  int n = 5;
-  int number = 12;
-  int expected = 4;
-  int result = solution(n, number);
+  vector<vector<int>> triangle = {
+      {7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}};
+  int expected = 30;
+  int result = solution(triangle);
 
   if (result == expected) {
     cout << "result: " << result << endl;
