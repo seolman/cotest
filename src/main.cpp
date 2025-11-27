9f33f83fec7fb4e072e3ade659c5077fbc030174
seolman <tjfehdgns@gmail.com>
2025-11-27 04:30:03 +0000

    dp, state management
    max dp와 min dp 테이블을 사용해서 +, - 일때 최대값, 최소값을 구할수 있게 한다.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <algorithm>
+#include <climits>
 #include <iostream>
+#include <string>
 #include <vector>
 
 using namespace std;
@@ -16,32 +18,49 @@ template <typename T> ostream &operator<<(ostream &os, vector<T> &vec) {
   return os;
 }
 
-int rob(vector<int> &money) {
-  int prev1 = 0;
-  int prev2 = 0;
-  for (const int &m : money) {
-    int current_max = max(prev1, m + prev2);
-    prev2 = prev1;
-    prev1 = current_max;
+int solution(vector<string> arr) {
+  vector<int> nums;
+  vector<char> ops;
+  for (int i = 0; i < arr.size(); i++) {
+    if (i % 2 == 0) {
+      nums.push_back(stoi(arr[i]));
+    } else {
+      ops.push_back(arr[i][0]);
+    }
   }
 
-  return prev1;
-}
+  const int n = nums.size();
+  vector<vector<int>> max_dp(n, vector<int>(n, INT_MIN));
+  vector<vector<int>> min_dp(n, vector<int>(n, INT_MAX));
 
-int solution(vector<int> money) {
-  vector<int> case1(money.begin(), money.end() - 1);
-  int max1 = rob(case1);
-  vector<int> case2(money.begin() + 1, money.end());
-  int max2 = rob(case2);
-  return max(max1, max2);
+  for (int i = 0; i < n; i++) {
+    max_dp[i][i] = nums[i];
+    min_dp[i][i] = nums[i];
+  }
+
+  for (int len = 2; len < n + 1; len++) {
+    for (int i = 0; i < n - len + 1; i++) {
+      int j = i + len - 1;
+      for (int k = i; k < j; k++) {
+        if (ops[k] == '+') {
+          max_dp[i][j] = max(max_dp[i][j], max_dp[i][k] + max_dp[k + 1][j]);
+          min_dp[i][j] = min(min_dp[i][j], min_dp[i][k] + min_dp[k + 1][j]);
+        } else {
+          max_dp[i][j] = max(max_dp[i][j], max_dp[i][k] - min_dp[k + 1][j]);
+          min_dp[i][j] = min(min_dp[i][j], min_dp[i][k] - max_dp[k + 1][j]);
+        }
+      }
+    }
+  }
+  return max_dp[0][n - 1];
 }
 
 int main() {
   cout << "TESTING" << endl;
 
-  vector<int> money = {1, 2, 3, 1};
-  int expected = 4;
-  int result = solution(money);
+  vector<string> arr = {"1", "-", "3", "+", "5", "-", "8"};
+  int expected = 1;
+  int result = solution(arr);
 
   if (result == expected) {
     cout << "result: " << result << endl;
