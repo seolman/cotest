8c57be7c06d4362b664e56e2d446a93f9d24c04c
seolman <tjfehdgns@gmail.com>
2025-11-27 03:16:57 +0000

    dp, memorization

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
 
@@ -16,29 +15,42 @@ template <typename T> ostream &operator<<(ostream &os, vector<T> &vec) {
   return os;
 }
 
-int solution(vector<vector<int>> triangle) {
-  for (int i = 1; i < triangle.size(); i++) {
-    for (int j = 0; j < triangle[i].size(); j++) {
-      if (j == 0) {
-        triangle[i][j] += triangle[i - 1][j];
-      } else if (j == triangle[i].size() - 1) {
-        triangle[i][j] += triangle[i - 1][j - 1];
-      } else {
-        triangle[i][j] += max(triangle[i - 1][j], triangle[i - 1][j - 1]);
+int solution(int m, int n, vector<vector<int>> puddles) {
+  int mod = 1000000007;
+
+  vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+  for (const auto &puddle : puddles) {
+    dp[puddle[1]][puddle[0]] = -1;
+  }
+
+  dp[1][1] = 1;
+
+  for (int i = 1; i < n + 1; i++) {
+    for (int j = 1; j < m + 1; j++) {
+      if (dp[i][j] == -1) {
+        dp[i][j] = 0;
+        continue;
+      }
+
+      if (i > 1) {
+        dp[i][j] = (dp[i][j] + dp[i - 1][j]) % mod;
+      }
+      if (j > 1) {
+        dp[i][j] = (dp[i][j] + dp[i][j - 1]) % mod;
       }
     }
   }
-  int answer = *max_element(triangle.back().begin(), triangle.back().end());
-  return answer;
+  return dp[n][m];
 }
 
 int main() {
   cout << "TESTING" << endl;
 
-  vector<vector<int>> triangle = {
-      {7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}};
-  int expected = 30;
-  int result = solution(triangle);
+  int m = 4;
+  int n = 3;
+  vector<vector<int>> puddles = {{2, 2}};
+  int expected = 4;
+  int result = solution(m, n, puddles);
 
   if (result == expected) {
     cout << "result: " << result << endl;
