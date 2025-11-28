18311fb3c10e31dd41296af03a90362020c679b5
seolman <tjfehdgns@gmail.com>
2025-11-28 00:27:44 +0000

    simulation, greedy
    무한 루프로 가장 긴 값과 가장 짧은 값을 구해 조건문으로 돌린다고 가정하고 결정한다.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,30 @@
-#include <algorithm>
-#include <climits>
 #include <iostream>
-#include <string>
 #include <vector>
 
 using namespace std;
+
+int solution(vector<int> wallet, vector<int> bill) {
+  int answer = 0;
+  int w_max = max(wallet[0], wallet[1]);
+  int w_min = min(wallet[0], wallet[1]);
+  while (true) {
+    int bill_w = bill[0];
+    int bill_h = bill[1];
+
+    if ((bill_w <= w_max && bill_h <= w_min) ||
+        (bill_h <= w_max && bill_w <= w_min)) {
+      break;
+    }
+
+    if (bill_w >= bill_h) {
+      bill[0] /= 2;
+    } else {
+      bill[1] /= 2;
+    }
+    answer++;
+  }
+  return answer;
+}
 
 template <typename T> ostream &operator<<(ostream &os, vector<T> &vec) {
   os << "{";
@@ -18,49 +38,13 @@ template <typename T> ostream &operator<<(ostream &os, vector<T> &vec) {
   return os;
 }
 
-int solution(vector<string> arr) {
-  vector<int> nums;
-  vector<char> ops;
-  for (int i = 0; i < arr.size(); i++) {
-    if (i % 2 == 0) {
-      nums.push_back(stoi(arr[i]));
-    } else {
-      ops.push_back(arr[i][0]);
-    }
-  }
-
-  const int n = nums.size();
-  vector<vector<int>> max_dp(n, vector<int>(n, INT_MIN));
-  vector<vector<int>> min_dp(n, vector<int>(n, INT_MAX));
-
-  for (int i = 0; i < n; i++) {
-    max_dp[i][i] = nums[i];
-    min_dp[i][i] = nums[i];
-  }
-
-  for (int len = 2; len < n + 1; len++) {
-    for (int i = 0; i < n - len + 1; i++) {
-      int j = i + len - 1;
-      for (int k = i; k < j; k++) {
-        if (ops[k] == '+') {
-          max_dp[i][j] = max(max_dp[i][j], max_dp[i][k] + max_dp[k + 1][j]);
-          min_dp[i][j] = min(min_dp[i][j], min_dp[i][k] + min_dp[k + 1][j]);
-        } else {
-          max_dp[i][j] = max(max_dp[i][j], max_dp[i][k] - min_dp[k + 1][j]);
-          min_dp[i][j] = min(min_dp[i][j], min_dp[i][k] - max_dp[k + 1][j]);
-        }
-      }
-    }
-  }
-  return max_dp[0][n - 1];
-}
-
 int main() {
   cout << "TESTING" << endl;
 
-  vector<string> arr = {"1", "-", "3", "+", "5", "-", "8"};
+  vector<int> wallet = {30, 15};
+  vector<int> bill = {26, 17};
   int expected = 1;
-  int result = solution(arr);
+  int result = solution(wallet, bill);
 
   if (result == expected) {
     cout << "result: " << result << endl;
